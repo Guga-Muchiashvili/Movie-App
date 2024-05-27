5cccf746effc29bc598673383b841a0c92c84057
guga-ggw <ggwomega15@gmail.com>
2024-05-27 12:09:51 +0400

    adding new project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.scss'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-red-700 '>Hello</h1>
+    </>
+  )
+}
+
+export default App
